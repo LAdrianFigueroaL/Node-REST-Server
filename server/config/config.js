6525120490819df376ec00d03d61f809dfc349bf
Adrian Figueroa <neo.adrian@gmail.com>
2018-07-20 20:38:57 -0500

    Variable de Entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:a123456@ds147011.mlab.com:47011/cafe_neo';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
